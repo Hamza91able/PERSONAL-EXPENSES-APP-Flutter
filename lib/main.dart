bfae81db1429ac35625dbcde125e03bb760a96b1
Hamza91able <m.hamzakhan91able@gmail.com>
2020-06-02 16:08:03 +0500

    Styling a Container

--- lib/main.dart
+++ lib/main.dart
@@ -54,13 +54,26 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                 children: <Widget>[
                   Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
                     child: Text(
                       tx.amount.toString(),
                     ),
                   ),
                   Column(
                     children: <Widget>[
-                      Text(tx.title),
+                      Text(
+                        tx.title,
+                      ),
                       Text(
                         tx.date.toString(),
                       ),
