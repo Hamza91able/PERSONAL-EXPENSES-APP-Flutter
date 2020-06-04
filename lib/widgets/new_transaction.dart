186bc563622fc52203807097a91d4c7271de925d
Hamza91able <m.hamzakhan91able@gmail.com>
2020-06-04 18:23:30 +0500

    Removed line break

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
 import './adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
