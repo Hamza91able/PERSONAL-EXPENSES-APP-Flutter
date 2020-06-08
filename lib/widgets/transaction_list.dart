c50a5967e2a8a68f5b4fdaeca230be8012040e29
Hamza91able <m.hamzakhan91able@gmail.com>
2020-06-08 13:09:40 +0500

    Extracting Widgets

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+
+import './transaction_card.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,44 +36,7 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text('\$${transactions[index].amount}')),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 460
-                      ? FlatButton.icon(
-                          onPressed: () =>
-                              deleteTransaction(transactions[index].id),
-                          icon: const Icon(Icons.delete),
-                          label: const Text("Delete"),
-                          textColor: Theme.of(context).errorColor,
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () =>
-                              deleteTransaction(transactions[index].id),
-                          color: Theme.of(context).errorColor,
-                        ),
-                ),
-              );
+              return TransactionCard(transaction: transactions[index], deleteTransaction: deleteTransaction);
             },
           );
   }
